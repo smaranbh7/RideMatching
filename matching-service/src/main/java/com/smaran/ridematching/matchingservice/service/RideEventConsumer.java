dc083316c5fd7d0bd8b8c84c1a3e58c310d81984
smar7n <smaran10.bhattarai@gmail.com>
2026-05-02 17:31:15 -0400

    RideEventConsumer implemented

--- matching-service/src/main/java/com/smaran/ridematching/matchingservice/service/RideEventConsumer.java
+++ matching-service/src/main/java/com/smaran/ridematching/matchingservice/service/RideEventConsumer.java
@@ -1,4 +1,29 @@
 package com.smaran.ridematching.matchingservice.service;
 
-public class RIdeEventConsumer {
+import com.smaran.ridematching.matchingservice.event.RideRequestedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+//Listens to ride.requested kafka topic and is triggered every time ride service publishes new ride request
+public class RideEventConsumer {
+
+    private final MatchingService matchingService;
+
+    @KafkaListener(
+            topics = "ride.requested",
+            groupId = "matching-service-group"
+    )
+    public void consumeRideRequestedEvent (RideRequestedEvent event){
+        try{
+            matchingService.matchDriverForRide(event);
+        }catch (Exception e){
+            log.error("Error processing ride request: {} - {}", event.getRideId(), e.getMessage());
+        }
+    }
+
 }
