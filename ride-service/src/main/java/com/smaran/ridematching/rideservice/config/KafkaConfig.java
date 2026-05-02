3e4575119c47c6c519173bd8680ce2c9f55f96aa
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2026-05-02 15:06:24 -0400

    Kafka config

--- ride-service/src/main/java/com/smaran/ridematching/rideservice/config/KafkaConfig.java
+++ ride-service/src/main/java/com/smaran/ridematching/rideservice/config/KafkaConfig.java
@@ -1,4 +1,29 @@
 package com.smaran.ridematching.rideservice.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaConfig {
+
+    //Topic for ride service to publish ride request subscribed by matching service
+    @Bean
+    public NewTopic rideRequestedTopic(){
+        return TopicBuilder.name("ride.requested")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    //Topic for matching service to publish match results to be subscribed by ride service
+    @Bean
+    public NewTopic rideMatchedTopic(){
+        return TopicBuilder.name("ride.matched")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
 }
